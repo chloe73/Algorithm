55776613e6096257e4c89e68b3528b127a5c6f1e
chloe73 <esom735@gmail.com>
2022-12-19 23:43:28 +0900

    day1219 commit

---
+++ Main_bj_7579_앱.java
@@ -0,0 +1,13 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_bj_7579_앱 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
---
+++ binarySearch/Main_bj_2632_피자판매.java
@@ -0,0 +1,10 @@
+package algo.binarySearch;
+
+public class Main_bj_2632_피자판매 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
