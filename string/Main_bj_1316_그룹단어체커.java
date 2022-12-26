8d3f3a16ca8c8de4e0aed3402db81964956e6c4a
chloe73 <esom735@gmail.com>
2022-12-27 00:36:45 +0900

    1227 commit

--- string/Main_bj_1316_그룹단어체커.java
+++ string/Main_bj_1316_그룹단어체커.java
@@ -1,10 +1,26 @@
 package algo.string;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main_bj_1316_그룹단어체커 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		String[] input = new String[N];
+		Set<Character> alpha = new HashSet<Character>();
+		for(int i=0;i<N;i++) {
+			input[i] = br.readLine();
+			for(int j=0;j<input[i].length();j++) {
+				alpha.add(input[i].charAt(j));
+			}
+		}
 	}
 
 }
