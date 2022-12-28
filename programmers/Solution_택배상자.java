e037988d9bc14c3225945cc4d8b9804d3bc6e6dc
chloe73 <esom735@gmail.com>
2022-12-29 04:03:06 +0900

    1229 commit

--- programmers/Solution_택배상자.java
+++ programmers/Solution_택배상자.java
@@ -8,45 +8,36 @@ public class Solution_택배상자 {
     public int solution(int[] order) {
         int answer = 0;
         
-        ArrayList<Integer> truck = new ArrayList<>();
-        Queue<Integer> belt = new LinkedList<>();
+        Stack<Integer> truck = new Stack<>();
         
-        // order의 맨 처음 숫자를 기준으로 
-        int temp = order[0];
-        truck.add(temp);
-        
-        // order의 첫번째 번호보다 큰 수는 컨테이너 벨트에 추가
-        for(int i=order.length;i>temp;i--) {
-            belt.add(i);
+        int num = 1;
+        int idx = 0; // order 인덱스 번호
+        while(true) {
+            
+        	if(!truck.isEmpty() && order[idx] == truck.peek()) {
+        		answer++;
+        		
+        		idx++;
+        		truck.pop();
+        		
+        		continue;
+        	}
+        	
+        	if(num > order.length) break;
+        	
+        	if(order[idx] == num) {
+        		answer++;
+        		
+        		idx++;
+        		num++;
+        		
+        		continue;
+        	}
+            
+            truck.push(num);
+            num++;
         }
-        
-        // 보조 컨테이너 벨트에 order의 첫번째 번호보다 작은 수 추가
-        Queue<Integer> q = new LinkedList<>();
-        if(temp>1) {
-            for(int i=temp-1;i>0;i--) {
-                q.add(i);
-            }
-        }
-        
-        int idx = 1; // order 인덱스 번호
-        while(idx < order.length) {
-            
-            int next = order[idx];
-            
-            if(next == q.peek()) {
-                truck.add(q.poll());
-            }
-            
-            else if(next == belt.peek()) {
-                truck.add(belt.poll());
-            }
-            
-            else break;
-            
-            idx++;
-        }
-        
-        answer = truck.size();
+
         return answer;
     }
 
