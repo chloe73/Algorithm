72da49b929d2438f10a33b3f2c1c4a318e175d57
chloe73 <esom735@gmail.com>
2023-01-09 17:55:37 +0900

    0109 commit

---
+++ SW_DX/Solution_현석이의생일.java
@@ -0,0 +1,44 @@
+package algo.SW_DX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_현석이의생일 {
+
+    static int N,x,y,result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int tc=1;tc<=T;tc++) {
+
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            result = Integer.MIN_VALUE;
+
+            binarySearch();
+
+            sb.append("#"+tc+" ").append(result).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void binarySearch() {
+
+        int mid;
+        int low = 0;
+        int high = N;
+
+        while(low <= high) {
+            mid = (low+high) / 2;
+        }
+    }
+}
