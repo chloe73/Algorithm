f0fd8b8dbabee4a65b04ca52540794b24233c190
chloe73 <esom735@gmail.com>
2023-01-16 21:28:51 +0900

    1016 commit

---
+++ Main_bj_1238_파티.java
@@ -0,0 +1,16 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_bj_1238_파티 {
+	
+	static int N,result;
+	
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
