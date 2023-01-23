9dff23520281909c89cbf0a8b0d545676e329211
chloe73 <esom735@gmail.com>
2023-01-23 23:54:04 +0900

    0123 commit

---
+++ Main_bj_17837_새로운게임2.java
@@ -0,0 +1,29 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_bj_17837_새로운게임2 {
+	
+	static int N,K,result;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static int[][] board;
+	static class Horse {
+		int x,y,d;
+		public Horse(int x, int y, int d) {
+			this.x = x;
+			this.y = y;
+			this.d = d;
+		}
+	}
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+
+	}
+
+}
