32a6f37efae3045c9ed907492288230d6f62371f
esom735 <esom735@gmail.com>
2023-01-31 08:57:40 +0900

    0131 commit

---
+++ Main_bj_3109_빵집.java
@@ -0,0 +1,46 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_bj_3109_빵집 {
+	
+	static int R,C,result;
+	static char[][] board;
+	static int[] dx = {0,-1,1};
+	static int[] dy = {1,1,1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		board = new char[R][C];
+		for(int i=0;i<R;i++) {
+			String s = br.readLine();
+			board[i] = s.toCharArray();
+		} // input end
+		
+		result = Integer.MIN_VALUE;
+		solve(0,0,0);
+		
+		System.out.println(result);
+	}
+
+	private static void solve(int x, int y, int cnt) {
+		
+		if(y == C-1) { // 현재 열이 마지막 열이라면 
+			result = Math.max(result, cnt);
+			return;
+		}
+		
+		
+		
+	}
+
+}
