6de402b78c7e4021b798ce8419be4d241f684c74
chloe73 <esom735@gmail.com>
2023-04-19 09:31:13 +0900

    mysql 특정 옵션이 포함된 자동차 리스트 구하기 solve

--- mysql/특정 옵션이 포함된 자동차 리스트 구하기.sql
+++ mysql/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -3,3 +3,13 @@ SELECT *
 from CAR_RENTAL_COMPANY_CAR
 where options like '%네비게이션%'
 order by car_id desc;
+
+-- locate를 활용한 방법
+SELECT
+    car_id, car_type, daily_fee, options
+FROM
+    car_rental_company_car
+WHERE
+    locate('네비게이션', options)
+ORDER BY
+    car_id desc;
