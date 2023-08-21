899c41cf2bebeb41620e3b490acadc9671cce3af
chloe73 <esom735@gmail.com>
2023-08-21 21:15:18 +0900

    ✅ 0821 codetree 자율주행 전기차 solved

--- CodeTree/Main_ct_자율주행_전기차.java
+++ CodeTree/Main_ct_자율주행_전기차.java
@@ -14,6 +14,8 @@ public class Main_ct_자율주행_전기차 {
 	static int N,M,C,result;
 	static int CX,CY; // 자율주행 전기차 위치
 	static int[][] board;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
 	static HashMap<Integer, Passenger> passengerMap;
 	static class Point implements Comparable<Point> {
 		int num;
@@ -94,55 +96,35 @@ public class Main_ct_자율주행_전기차 {
 			// 승객이 여러명일 경우 현재 위치에서 최단 거리가 가장 짧은 승객을 먼저 태웁니다. 
 			// 만약 그런 승객이 여러 명일 경우에는 가장 위에 있는 승객을, 
 			// 그런 승객이 여러 명일 때는 가장 왼쪽에 있는 승객을 고릅니다.
+			int idx = find_passenger();
 			
-			PriorityQueue<Point> pq = new PriorityQueue<>();
-			
-			for(int i :passengerMap.keySet()) {
-				Passenger p = passengerMap.get(i);
-				
-				int dist = bfs(CX, CY, p.sx,p.sy);
-				
-				if(dist != -1) {
-					pq.add(new Point(i, p.sx, p.sy, dist));
-				}
-			}
-			
-			if(pq.size() == 0) {
+			if(idx == -1) {
 				result = -1;
 				return;
 			}
 			
-			// 2. 해당 승객 위치로 전기차 이동
-			Point target = pq.poll();
-			int dist = target.dist;
+			int ex = passengerMap.get(idx).ex;
+			int ey = passengerMap.get(idx).ey;
 			
-			if(C < dist) {
-				result = -1;
-				return;
-			}
-			
-			int ex = passengerMap.get(target.num).ex;
-			int ey = passengerMap.get(target.num).ey;
-			
-			int plusDist = bfs(target.x, target.y, ex, ey);
+			int plusDist = bfs(CX, CY, ex, ey);
 			
 			if(plusDist == -1) {
 				result = -1;
 				return;
 			}
 			
-			if(C < dist + plusDist) {
+			if(C < plusDist) {
 				result = -1;
 				return;
 			}
 			
 			// 해당 승객을 도착지까지 문제없이 데려다 줄 수 있는 경우
-			if(C >= dist + plusDist) {
+			if(C >= plusDist) {
 				CX = ex;
 				CY = ey;
-				C -= dist + plusDist;
+				C -= plusDist;
 				C += plusDist*2;
-				passengerMap.remove(target.num);
+				passengerMap.remove(idx);
 			}
 			// 불가능한 경우
 			else {
@@ -154,9 +136,62 @@ public class Main_ct_자율주행_전기차 {
 		result = C;
 	}
 	
+	private static int find_passenger() {
+		int index = -1;
+		
+		PriorityQueue<Point> pq = new PriorityQueue<>();
+		
+		int[][] copyBoard = new int[N][N];
+		
+		for(int i :passengerMap.keySet()) {
+			Passenger p = passengerMap.get(i);
+			
+			copyBoard[p.sx][p.sy] = i;
+		}
+		
+		Queue<int[]> q = new LinkedList<>();
+		boolean[][] visited = new boolean[N][N];
+		q.add(new int[] {CX,CY,0});
+		visited[CX][CY] = true;
+		
+		while(!q.isEmpty()) {
+			int[] temp = q.poll();
+			int x = temp[0];
+			int y = temp[1];
+			int dist = temp[2];
+			
+			if(copyBoard[x][y] > 0) pq.add(new Point(copyBoard[x][y], x, y, dist));
+			
+			for(int i=0;i<4;i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
+				if(!is_valid(nx, ny) || visited[nx][ny] || board[nx][ny] == 1) continue;
+				
+				visited[nx][ny] = true;
+				q.add(new int[] {nx,ny,dist+1});
+			}
+		}
+		
+		if(pq.size() == 0) return index;
+		
+		if(pq.size() > 0) {
+			Point p = pq.poll();
+			
+			if(C < p.dist) return index;
+			else {
+				C -= p.dist;
+				CX = p.x;
+				CY = p.y;
+				index = p.num;
+				return index;
+			}
+		}
+		
+		return index;
+	}
+
 	private static int bfs(int sx, int sy, int ex, int ey) {
-		int[] dx = {-1,1,0,0};
-		int[] dy = {0,0,-1,1};
 		int distance = -1;
 		
 		Queue<int[]> q = new LinkedList<>();
