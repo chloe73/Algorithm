de72e35ae08c7156121fa65a6c21ac3b71a9dcb2
chloe73 <esom735@gmail.com>
2023-10-29 23:23:23 +0900

    ✅ BOJ 1976 여행가자 solved

--- bj/Main_bj_1976_여행_가자.java
+++ bj/Main_bj_1976_여행_가자.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_bj_1976_여행가자 {
+public class Main_bj_1976_여행_가자 {
 	
 	static int N,M;
 	static int[] parent;
-	static boolean flag;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-
+		
 		parent = new int[N+1];
 		for(int i=1;i<=N;i++) {
 			parent[i] = i;
@@ -26,46 +26,49 @@ public class Main_bj_1976_여행가자 {
 		for(int i=1;i<=N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=1;j<=N;j++) {
-				int num = Integer.parseInt(st.nextToken());
-				if(num == 1) {
-					union(i,j);
+				int temp = Integer.parseInt(st.nextToken());
+				
+				if(temp == 1) {
+					union(i, j);
 				}
 			}
 		}
-		flag = true;
+		
+		boolean flag = true;
 		st = new StringTokenizer(br.readLine());
 		int start = find(Integer.parseInt(st.nextToken()));
 		for(int i=1;i<M;i++) {
-			int now = Integer.parseInt(st.nextToken());
+			int temp = Integer.parseInt(st.nextToken());
 			
-			if(start != find(now)) {
+			if(start != find(temp)) {
 				flag = false;
 				break;
 			}
 		}
 		
-		if(flag) System.out.println("YES");
-		else System.out.println("NO");
+		if(flag)
+			sb.append("YES");
+		else
+			sb.append("NO");
+			
+		System.out.println(sb.toString());
 	}
-
+	
 	private static int find(int x) {
-		if(x == parent[x]) {
-			return x;
-		}
+		if(x == parent[x]) return x;
 		
 		return parent[x] = find(parent[x]);
 	}
 	
-	public static void union(int x, int y) {
+	private static void union(int x, int y) {
 		x = find(x);
 		y = find(y);
 		
 		if(x != y) {
-			if(x < y) {
+			if(x < y)
 				parent[y] = x;
-			} else {
+			else
 				parent[x] = y;
-			}
 		}
 	}
 
