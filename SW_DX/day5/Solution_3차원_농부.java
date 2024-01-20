ad76fd402764bd636d996de60dd64206f1ff1b2b
chloe73 <esom735@gmail.com>
2024-01-20 16:37:09 +0900

    ✅ 3차원 농부 solved - 이분탐색

--- SW_DX/day5/Solution_3차원_농부.java
+++ SW_DX/day5/Solution_3차원_농부.java
@@ -37,17 +37,44 @@ public class Solution_3차원_농부 {
 			}
 			Arrays.sort(cow); // 이분 탐색을 위한 정렬
 			
-			horse = new int[M];
-			for(int i=0;i<M;i++) {
-				horse[i] = Integer.parseInt(st.nextToken());
-				
-				// 이분 탐색 수행
-				
-			}
-			
-			
-		}
+			int minDistance = Integer.MAX_VALUE;
+            int count = 0;
 
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<M;i++) {
+				int hz = Integer.parseInt(st.nextToken());
+
+				// 이분 탐색 수행
+				int left = 0;
+				int right = N-1;
+				
+				while(left <= right) {
+					int mid = (left + right) / 2;
+					
+					if(Math.abs(cow[mid]-hz) < minDistance) {
+						minDistance = Math.abs(cow[mid]-hz);
+						count = 1;
+					}
+					else if(Math.abs(cow[mid]-hz) == minDistance) {
+						count++;
+					}
+					
+					if(hz < cow[mid]) {
+						right = mid;
+					}
+					else if(hz > cow[mid]) {
+						left = mid+1;
+					}
+					else break;
+				}
+			}
+			minDistance += Math.abs(c1-c2);
+			bw.write(minDistance + " " + count+"\n");
+		}
+		
+		bw.flush();
+        br.close();
+        bw.close();
 	}
 
 }
