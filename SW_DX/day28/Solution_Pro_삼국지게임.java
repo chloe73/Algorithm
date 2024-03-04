be89aad8ea20cbbf2926f14922b2f4cab051686c
chloe73 <esom735@gmail.com>
2024-03-04 23:58:41 +0900

    ✔️ BOJ 4358 생태학 solving - 문자열

---
+++ SW_DX/day28/Solution_Pro_삼국지게임.java
@@ -0,0 +1,7 @@
+package algo.SW_DX.day28;
+
+public class Solution_Pro_삼국지게임 {
+
+	
+
+}
---
+++ bj/Main_bj_4358_생태학.java
@@ -0,0 +1,49 @@
+package algo.bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+
+public class Main_bj_4358_생태학 {
+	
+	static class Tree implements Comparable<Tree>{
+		String name;
+		int cnt;
+		public Tree(String name, int cnt) {
+			this.name = name;
+			this.cnt = cnt;
+		}
+		public int compareTo(Tree o) {
+			return this.name.compareTo(o.name);
+		}
+	}
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		TreeMap<String, Integer> map = new TreeMap<>();
+		int total = 0;
+		String input;
+		while((input = br.readLine()) != null) {
+			
+			if(!map.containsKey(input)) {
+				map.put(input, 1);
+			}
+			else {
+				map.put(input, map.get(input)+1);
+			}
+			
+			total++;
+		} // input end
+		
+		for(String s : map.keySet()) {
+			sb.append(s+" ");
+			sb.append(String.format("%.4f", map.get(s) / total)+"\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
