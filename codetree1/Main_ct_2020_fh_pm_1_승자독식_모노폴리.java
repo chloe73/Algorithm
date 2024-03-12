c9f6a9ac73146efde62a6ef5adbb23f530b020c1
chloe73 <esom735@gmail.com>
2024-03-12 22:23:34 +0900

    ✔️ codetree 승자독식 모노폴리 solving

---
+++ codetree1/Main_ct_2020_fh_pm_1_승자독식_모노폴리.java
@@ -0,0 +1,17 @@
+package algo.codetree1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_ct_2020_fh_pm_1_승자독식_모노폴리 {
+	
+	static int N,K;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+
+}
