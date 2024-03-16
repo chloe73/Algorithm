b18712424824286f0e0ef1d957f9c516e9b4f907
chloe73 <esom735@gmail.com>
2024-03-16 20:57:08 +0900

    ✅ BOJ 5525 IOIOI solved

--- bj/Main_bj_5525_IOIOI.java
+++ bj/Main_bj_5525_IOIOI.java
@@ -7,27 +7,28 @@ import java.io.InputStreamReader;
 public class Main_bj_5525_IOIOI {
 	
 	static int N,M,result;
-	static String s;
-	static String target = "IOI";
+	static char[] arr;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-		s = br.readLine();
+		arr = br.readLine().toCharArray();
 		result = 0;
 		
-		while(--N > 0) {
-			target += "OI";
-		}
-		
-		// OOIOIOIOIIOII
-		for(int i=0;i<=M-target.length();i++) {
-			String temp = s.substring(i, i+target.length());
-			
-			if(temp.equals(target))
-				result++;
+		// N = 2
+		// OO IOIOI OIIOII
+		// OOIO IOIOI IOII
+		int[] memo = new int[M];
+		for(int i=1;i<M-1;i++) {
+			if(arr[i] == 'O' && arr[i+1] == 'I') {
+				memo[i+1] = memo[i-1] + 1;
+				
+				if(memo[i+1] >= N && arr[i-2*N+1] == 'I') {
+					result++;
+				}
+			}
 		}
 		
 		System.out.println(result);
