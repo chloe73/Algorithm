f61a5dfe07b43b08cb9b495cde7aac630aae95b7
chloe73 <esom735@gmail.com>
2024-03-18 00:24:32 +0900

    ✅ BOJ 1213 팰린드롬 만들기 solved

--- bj/Main_bj_1213_팰린드롬_만들기.java
+++ bj/Main_bj_1213_팰린드롬_만들기.java
@@ -8,37 +8,39 @@ public class Main_bj_1213_팰린드롬_만들기 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line = br.readLine();
 		int[] alpha = new int[26];
-		for(int i=0; i<line.length(); i++) {
-			int idx = line.charAt(i)-'A';
-			alpha[idx]++;
+		String s = br.readLine();
+		for(int i=0;i<s.length();i++) {
+			int temp = s.charAt(i) - 'A';
+			alpha[temp]++;
+		} // input end
+		
+		int hol = 0;
+		for(int i=0;i<alpha.length;i++) {
+			if(alpha[i] % 2 != 0) hol++;
 		}
 		
-		int isOne =0;
-		for(int i=0; i<alpha.length; i++) {
-			if(alpha[i]%2!=0) isOne++;
-		}
-		
-		String answer ="";
+		String answer = "";
 		StringBuilder sb = new StringBuilder();
-		if(isOne>1) answer += "I'm Sorry Hansoo";
+		if(hol > 1)
+			answer += "I'm Sorry Hansoo";
 		else {
-			for(int i=0; i<alpha.length; i++) {
-				for(int r=0; r<alpha[i]/2; r++) {
-					sb.append((char)(i+65));
+			for(int i=0;i<26;i++) {
+				for(int j=0;j<alpha[i]/2;j++) {
+					sb.append((char) (i+'A'));
 				}
 			}
+			
 			answer += sb.toString();
 			String end = sb.reverse().toString();
-			
 			sb = new StringBuilder();
-			for(int i=0; i<alpha.length; i++) {
-				if(alpha[i]%2==1) {
-					sb.append((char)(i+65));
+			
+			for(int i=0;i<26;i++) {
+				if(alpha[i]%2 == 1) {
+					sb.append((char) (i+'A'));
 				}
 			}
-			answer +=sb.toString()+end;
+			answer += sb.toString() + end;
 		}
 		System.out.println(answer);
 	}
