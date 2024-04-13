2de05699983007d813f85a04dc0d8fec5d26c02f
chloe73 <esom735@gmail.com>
2024-04-13 23:13:50 +0900

    📓 [Java] 2차원 부분 배열 돌리기

--- codetree1/Main_rotate.java
+++ codetree1/Main_rotate.java
@@ -3,6 +3,7 @@ package algo.codetree1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_rotate {
@@ -61,6 +62,34 @@ public class Main_rotate {
 			}
 			System.out.println();
 		}
+		
+		System.out.println(" ======= 부분 시계 방향 90도 회전 ======= ");
+		arr2 = rotate(1, 2, 3, 3);
+		for(int i=0;i<M;i++) {
+			for(int j=0;j<N;j++) {
+				System.out.print(arr2[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	private static int[][] rotate(int sx, int sy, int n, int m){
+		int[][] board = new int[M][N];
+		for(int i=0;i<N;i++) {
+			board[i] = Arrays.copyOf(arr[i], N);
+		}
+		
+		for(int i=sx;i<sx+n;i++) {
+			for(int j=sy;j<sy+m;j++) {
+				int ox = i-sx;
+				int oy = j-sy;
+				int rx = oy;
+				int ry = n-1-ox;
+				board[rx+sx][ry+sy] = arr[i][j];
+			}
+		}
+		
+		return board;
 	}
 
 }
