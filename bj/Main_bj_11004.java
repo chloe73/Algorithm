49ca6d695a203795329e857991bc20efea57d952
chloe73 <esom735@gmail.com>
2024-05-07 00:00:43 +0900

    ✅ BOJ 11004 solved - priority queue

--- bj/Main_bj_11004.java
+++ bj/Main_bj_11004.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main_bj_11004 {
@@ -16,14 +17,22 @@ public class Main_bj_11004 {
 		int K = Integer.parseInt(st.nextToken());
 		
 		int[] arr = new int[N];
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<N;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
+			pq.add(arr[i]);
 		} // input end
 		
-		Arrays.sort(arr);
-
-		System.out.println(arr[K-1]);
+//		Arrays.sort(arr); -> 시간초과 발생
+		
+		int result = 0;
+		int order = 0;
+		while(order++ < K) {
+			result = pq.poll();
+		}
+		System.out.println(result);
+//		System.out.println(arr[K-1]);
 	}
 
 }
