404ef984a9f2dd32a0fc92d2027628c0d6946563
chloe73 <esom735@gmail.com>
2024-05-12 22:42:05 +0900

    ✅ BOJ 11021 solved

---
+++ bj/Main_bj_11021.java
@@ -0,0 +1,27 @@
+package algo.bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_bj_11021 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=T;tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int result = A+B;
+			
+			sb.append("Case #"+tc+": ").append(result+"\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
